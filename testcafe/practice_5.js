9b5fe0a478d8618b1b2cf9b257667452279c9801
Joaquin Quintero Vizcarra <jquintero@vivaorganica.com.mx>
2020-10-25 11:26:24 -0700

    Finished practice 5 and modified practice 6

--- testcafe/practice_5.js
+++ testcafe/practice_5.js
@@ -8,16 +8,29 @@ fixture('Practice 5 with TestCafe')
             .click(page.inputs_Link)
 
         await t
-            .typeText(page.number_input, 'John Smith')
-            .expect(page.number_input.value).eql('')
-
             .typeText(page.number_input, '235345243524')
             .expect(page.number_input.value).notEql('3423')
             .expect(page.number_input.value).eql('235345243524')
+    });
 
+    test('Test with letters', async t =>{
+        await t
+            .click(page.inputs_Link)
+
+        await t
+            .typeText(page.number_input, 'John Smith')
+            .expect(page.number_input.value).eql('')
+    });
+
+    test('Test with numbers and replace funcion', async t =>{
+        await t
+            .click(page.inputs_Link)
+
+        await t
             .typeText(page.number_input, '666', {replace:true})
             .typeText(page.number_input, '!@#$%^&8*()_}{":?><', {replace:true})
             console.log(page.number_input.value)
         await t.expect(page.number_input.value).eql('8')
+        
     });
 
--- testcafe/practice_6.js
+++ testcafe/practice_6.js
@@ -3,14 +3,36 @@ import page from './pageModel';
 fixture('Practice 6 with TestCafe')
     .page('https://the-internet.herokuapp.com/');
 
-    test('Test with add and remove elements', async t =>{
+    test('Test with add and remove elements with parent selector', async t =>{
+        await t
+            .click(page.add_remove_Link)
+
+        await t
+            .expect(page.add_Btn.visible).ok()
+            .click(page.add_Btn)
+            .click(page.add_Btn)
+            .click(page.add_Btn)
+
+        
+        await t
+            .expect(page.remove_Btn.nth(1).visible).ok()
+            .expect(page.remove_Btn.nth(2).visible).ok()
+            .expect(page.remove_Btn.nth(3).visible).ok()
+    });
+
+    test('Test with add and remove elements with parent selector', async t =>{
         await t
             .click(page.add_remove_Link)
 
         await t
             .click(page.add_Btn)
+            .click(page.add_Btn)
+            .click(page.add_Btn)
+
         
         await t
-            .click(page.remove_Btn.nth(0))
+            .click(page.remove_Btn.nth(1))
+            .click(page.remove_Btn.nth(2))
+            .click(page.remove_Btn.nth(3))
     });
 
