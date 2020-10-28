18b14d91c259d30dda978a091c5766058b438f8b
Joaquin Quintero Vizcarra <jquintero@vivaorganica.com.mx>
2020-10-27 18:21:35 -0700

    Finished practice 11

--- testcafe/practice_11.js
+++ testcafe/practice_11.js
@@ -11,5 +11,16 @@ fixture('Practice 11 with TestCafe')
             .click(page.horizontal_slider)
             .typeText(page.horizontal_slider, "3.5",{speed:0.1})
             .expect(page.horizontal_slider.value).eql('3.5')
+    });
 
+    test('Testing horizontal slider with arrows', async t =>{
+        await t
+            .click(page.horizontal_slider_Link)
+        
+        await t
+            .click(page.horizontal_slider)
+            .pressKey('RIGHT')
+            .pressKey('RIGHT')
+            .pressKey('RIGHT')
+            .expect(page.horizontal_slider.value).eql('1.5')
     });
